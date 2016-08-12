17d240d8ba11da06733826417e9ca340b5f03549
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-12 14:30:20 -0400

    Renamed 'Context' to 'ContextMenu'

---
+++ src/components/ContextMenu/ContextMenu.js
@@ -0,0 +1,55 @@
+function ContextMenu() {
+  this.elements = [];
+
+  this.node = {
+    document : el({ class : 'context-menu' })
+  };
+
+  this.node.document.append(
+    this.node.content = el({ class : 'context-menu_content' })
+  );
+}
+
+Component.extend(ContextMenu);
+
+ContextMenu.prototype.close = function () {
+  var self = this;
+
+  this.node.document.fadeOut(700, function () {
+    self.node.document.remove();
+  });
+};
+
+ContextMenu.prototype.open = function (coordinates) {
+  var self = this;
+
+  var documentOffset;
+  var contextItemOffset;
+  var close;
+
+  this.node.document.appendTo('body');
+
+  if (coordinates) {
+    documentOffset = this.node.document.offset();
+    contextOffset = this.elements[0].offset();
+    this.node.document.style.left = coordinates.x - (documentOffset.width * 0.05) + 'px';
+    this.node.document.style.top = coordinates.y - (contextOffset.top + (contextOffset.height / 2)) + 'px';
+  }
+
+  document.body.addEventListener('click', close = function () {
+    self.close();
+    document.body.removeEventListener('click', close);
+  });
+};
+
+ContextMenu.prototype.append = function () {
+  var i = 0;
+  var n = arguments.length;
+  for (; i < n; i++) {
+    if (arguments[i].node.tagName === 'DIV') {
+      arguments[i].addClass('context-menu_item');
+    }
+    this.elements.push(arguments[i]);
+    this.node.content.append(arguments[i]);
+  }
+};
--- src/components/Key/key.prototype.drop.js
+++ src/components/Key/key.prototype.drop.js
@@ -1,22 +1,38 @@
-Key.prototype.drop = function () {
-  // Swap
-  // Replace
-  var m = el(Menu,
-    {
-      target : this.keyTarget.node.document
-    },
-    el(Menu.Item, {
-      text : 'Swap'
-    }),
-    el(Menu.Item, {
-      text : 'Replace'
-    }),
-    el(Menu.Item, {
-      text : 'Tap key'
-    }),
-    el(Menu.Item, {
-      text : 'Told key'
-    })
-  );
-  m.open();
+Key.prototype.drop = function (coordinates) {
+  var context = el(ContextMenu);
+  var self = this;
+
+  if (this.dropTarget.isTransparent) {
+    this.dropTarget.replaceWith(this);
+    this.clear();
+  } else {
+    context.on('close', function () {
+      key.dropTarget.lightOff();
+    });
+
+    context.append(
+      el({
+        onClick : function () {
+          self.dropTarget.replaceWith(self);
+          self.clear();
+        }
+      }, 'Replace'),
+      el({
+        onClick : function () {
+          self.dropTarget.replaceWith(self);
+        }
+      }, 'Replace and Copy')
+    );
+    if (this.dropTarget.isHoldLayerTapKey) {
+      context.append(
+        el('hr'),
+        el({
+          onClick : function () {
+            self.dropTarget.setTapKey(self);
+          }
+        }, 'Replace tap key')
+      );
+    }
+    context.open(coordinates);
+  }
 };
