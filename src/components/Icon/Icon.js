b1f874b4c866422ec42cb4de721684578140774f
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-12 14:48:45 -0400

    Remove icons when they exist and 'str_name' is falsey

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -6,17 +6,23 @@ function Icon() {
 }
 
 Icon.prototype.set = function (str_name) {
+  // Clear the class name of icons
+  this.node.document.node.className = this.node.document.node.className
+    .split(' ')
+    .filter(a => !/^icon-/.test(a))
+    .join(' ');
+
   if (str_name) {
-    this.node.document.node.className = this.node.document.node.className
-      .split(' ')
-      .filter(a => !/^icon-/.test(a))
-      .join(' ');
     this.node.document.addClass('icon-' + str_name);
     if (!this.node.img) {
       this.node.document.append(
         this.node.img = el('img', { src : 'bin/ergodox_icon.svg' })
       );
     }
+  } else if (this.node.img) {
+    // When icons are changed places, they sometimes go from having an icon
+    // to not having an icon
+    this.node.img.remove();
   }
 };
 
