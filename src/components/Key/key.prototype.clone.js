12eb263319de698d2c646dbbe288f340b34b13bd
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-12 14:49:25 -0400

    removed 'Layer.move' specific code.

--- src/components/Key/key.prototype.clone.js
+++ src/components/Key/key.prototype.clone.js
@@ -1,13 +1,3 @@
 Key.prototype.clone = function () {
-  var clone = new Key({ keyCode : this.keyCode });
-
-  clone.node.document.removeClass('key--giant');
-  clone.node.document.removeClass('key--tall');
-
-  clone.node.document.style.position = 'absolute';
-  clone.node.document.style.zIndex = 2;
-  clone.node.document.style.left = '0';
-  clone.node.document.style.top = '0';
-
-  return clone;
+  return new Key({ keyCode : this.keyCode });
 };
