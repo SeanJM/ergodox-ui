d8517e87ef52e1368ee57899b265a7e1891f79e4
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-12 21:16:19 -0400

    Language, added 'drop target key' text

--- src/scripts/custom/language/english.js
+++ src/scripts/custom/language/english.js
@@ -2,3 +2,4 @@ var lang = new Language('english');
 
 lang.set('duplicate layer name', 'Invalid layer name: **{{name}}**, this name is already taken. Please choose another name.');
 lang.set('confirm layer delete', 'Are you sure you want to delete layer **{{name}}**?');
+lang.set('error drop target key is locked', 'A layer toggle depends on this key. \nKeys are locked when their positions are referenced by layer toggles.');
