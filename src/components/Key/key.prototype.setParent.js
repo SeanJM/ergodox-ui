9012a0961e677aaa8291cbf2ecec745fbbd066cc
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-12 21:29:44 -0400

    setParent also sets boolean 'isLocked'

--- src/components/Key/key.prototype.setParent.js
+++ src/components/Key/key.prototype.setParent.js
@@ -4,5 +4,6 @@ Key.prototype.setParent = function (key) {
   this.str_iconPrimary = key.str_iconPrimary;
   this.str_iconSecondary = key.str_iconSecondary;
   this.str_primary = key.str_primary;
+  this.isLocked = true;
   this.draw();
 };
