21653197a3e1800db5c936a4d7407bc9effefe05
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-12 21:16:32 -0400

    Adjusted timing for animation

--- src/scripts/custom/index.js
+++ src/scripts/custom/index.js
@@ -1,56 +1,102 @@
 // Contains event listeners and reactions
 var state = new Odin();
 
-el.fn('fadeOut', function () {
-  var time;
-  var callback;
+el.fn('popFadeIn', function (time, callback) {
+  var self = this;
+  time = time || 600;
 
-  if (typeof arguments[0] === 'function') {
-    callback = arguments[0];
-  } else if (typeof arguments[0] === 'number') {
-    time = arguments[0];
-  }
-
-  if (typeof arguments[1] === 'function') {
-    callback = arguments[1];
-  } else if (typeof arguments[1] === 'number') {
-    time = arguments[1];
-  }
+  this.node.style.opacity = 0;
 
   anime({
-    targets : this.node,
-    opacity : [1, 0],
-    elasticity : 0,
-    duration : time || 300,
+    targets : self.node,
+    duration : time,
+    opacity : [0, 1],
+    ease : 'easeInExpo',
+  });
+
+  anime({
+    targets : self.node,
+    elasticity : 400,
     ease : 'easeOutQuad',
+    duration : time,
+    scale : [0, 1],
     complete : callback
   });
 });
 
-el.fn('fadeIn', function () {
-  var time;
-  var callback;
+el.fn('popFadeOut', function (callback) {
+  var self = this;
+
+  anime({
+    targets : self.node,
+    duration : 400,
+    opacity : [1, 0],
+    ease : 'linear'
+  });
+
+  anime({
+    targets : self.node,
+    ease : 'easeInExpo',
+    duration : 400,
+    scale : [1, 0],
+    complete : callback
+  });
+});
+
+el.fn('fadeOut', function () {
+  var opt = {
+    targets : this.node,
+    opacity : [1, 0],
+    elasticity : 0,
+    duration : 600,
+    ease : 'linear',
+  };
 
   if (typeof arguments[0] === 'function') {
-    callback = arguments[0];
+    opt.complete = arguments[0];
   } else if (typeof arguments[0] === 'number') {
-    time = arguments[0];
+    opt.duration = arguments[0];
+  } else if (typeof arguments[0] === 'object') {
+    _.assign(opt, arguments[0]);
   }
 
   if (typeof arguments[1] === 'function') {
-    callback = arguments[1];
+    opt.complete = arguments[1];
   } else if (typeof arguments[1] === 'number') {
-    time = arguments[1];
+    opt.duration = arguments[1];
+  } else if (typeof arguments[1] === 'object') {
+    _.assign(opt, arguments[1]);
   }
 
-  anime({
+  anime(opt);
+});
+
+el.fn('fadeIn', function () {
+  var opt = {
     targets : this.node,
     opacity : [0, 1],
     elasticity : 0,
-    duration : time || 300,
-    ease : 'easeOutQuad',
-    complete : callback
-  });
+    duration : 600,
+    ease : 'linear',
+  };
+
+  if (typeof arguments[0] === 'function') {
+    opt.complete = arguments[0];
+  } else if (typeof arguments[0] === 'number') {
+    opt.duration = arguments[0];
+  } else if (typeof arguments[0] === 'object') {
+    _.assign(opt, arguments[0]);
+  }
+
+  if (typeof arguments[1] === 'function') {
+    opt.complete = arguments[1];
+  } else if (typeof arguments[1] === 'number') {
+    opt.duration = arguments[1];
+  } else if (typeof arguments[1] === 'object') {
+    _.assign(opt, arguments[1]);
+  }
+
+  anime(opt);
 });
 
 el.fn('matrixRotation', function () {
