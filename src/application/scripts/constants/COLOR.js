7be87d16247cd33208b6f2e779b89c15129f07f6
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-26 08:33:01 -0400

    Changed the hue of 'red'

--- src/application/scripts/constants/COLOR.js
+++ src/application/scripts/constants/COLOR.js
@@ -1,6 +1,6 @@
 var COLOR = {
-  red : blush('#d23551'),
-  blue : blush('#d23551').rotate(205).hex(),
-  cyan : blush('#d23551').rotate(175).hex(),
-  green : blush('#d23551').rotate(115).darken(0.2).hex()
+  red : blush('#d92b55'),
+  blue : blush('#d92b55').rotate(205).hex(),
+  cyan : blush('#d92b55').rotate(175).hex(),
+  green : blush('#d92b55').rotate(115).darken(0.2).hex()
 };
