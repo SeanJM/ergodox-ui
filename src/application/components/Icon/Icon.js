b8038d38a2995c31424f5d79df78a2fe9111e371
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-26 21:42:53 -0400

    Cleaned up 'icon' component.

--- src/application/components/Icon/Icon.js
+++ src/application/components/Icon/Icon.js
@@ -1,29 +1,23 @@
 function Icon() {
   this.img = false;
+
   this.node = {
     document : el({ class : 'icon' })
   };
+
+  this.node.document.append(
+    this.node.img = el(ICON_SVG)
+  );
 }
 
-Icon.prototype.set = function (str_name) {
-  // Clear the class name of icons
-  this.node.document.node.className = this.node.document.node.className
+Icon.prototype.text = function (str_value) {
+  var className = this.node.document.node.className;
+
+  this.node.document.node.className = className
     .split(' ')
     .filter(a => !/^icon-/.test(a))
-    .join(' ');
-
-  if (str_name) {
-    this.node.document.addClass('icon-' + str_name);
-    if (!this.node.img) {
-      this.node.document.append(
-        this.node.img = el('img', { src : 'bin/ergodox_icon.svg' })
-      );
-    }
-  } else if (this.node.img) {
-    // When icons are changed places, they sometimes go from having an icon
-    // to not having an icon
-    this.node.img.remove();
-  }
+    .join(' ') +
+    ' icon-' + str_value;
 };
 
 Component.extend(Icon);
