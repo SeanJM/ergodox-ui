dcbc1eab4ed8c997f34a9db0496b3cb67597ed69
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-27 08:13:14 -0400

    Added 'active' input element which is a stroke to show focus. Added styles for icons in inputs.

--- src/application/components/Input/Input.js
+++ src/application/components/Input/Input.js
@@ -1,19 +1,51 @@
 function Input() {
+  var self = this;
+
   this.node = {
     document : el({ class : 'input' })
   };
+
+  this.node.document.append(
+    this.node.active = el({ class : 'input_active' })
+  );
+
+  this.on('focus', function () {
+    self.node.document.addClass('input--active');
+  });
+
+  this.on('blur', function () {
+    self.node.document.removeClass('input--active');
+  });
+
+  this.style = this.node.document.style;
 }
 
-Input.prototype.append = function (a) {
+Input.prototype.append = function () {
   var i = 0;
   var n = arguments.length;
+  var self = this;
+
+  function trigger(e) {
+    self.trigger(e.type);
+  }
 
   for (; i < n; i++) {
     if (arguments[i] instanceof Icon) {
       this.node.document.addClass('input--icon');
+    } else {
+      this.node.input = arguments[i];
+      this.node.input.on('focus, blur', trigger);
     }
     Component.prototype.append.call(this, arguments[i]);
   }
+};
+
+Input.prototype.offset = function () {
+  return this.node.document.offset();
+};
+
+Input.prototype.focus = function () {
+  this.node.input.focus();
 };
 
 Component.extend(Input);
