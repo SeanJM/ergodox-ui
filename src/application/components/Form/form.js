93422be28285f508f0b9d163162a2bf4e4596144
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-27 08:11:46 -0400

    Form component with support for 'bottom' setting on static elements.

--- src/application/components/Form/form.js
+++ src/application/components/Form/form.js
@@ -8,8 +8,28 @@ function Form() {
 Form.prototype.append = function () {
   var i = 0;
   var n = arguments.length;
+  var self = this;
+
+  function bottom(element) {
+    function resize() {
+      var formOffset = self.node.document.offset();
+      var inputTop = element.node.document.offset().top;
+      element.style.height = formOffset.height - element.bottom - (inputTop - formOffset.top) + 'px';
+    }
+
+    element.on('mount', function () {
+      resize();
+      window.addEventListener('resize', _.debounce(resize, 25));
+    });
+  }
+
   for (; i < n; i++) {
-    this.node.document.append(arguments[i]);
+    Component.prototype.append.call(this, arguments[i]);
+
+    if (typeof arguments[i].bottom === 'number') {
+      bottom(arguments[i]);
+    }
+
     if (typeof arguments[i].validate === 'function') {
       this.elements.push(arguments[i]);
     }
