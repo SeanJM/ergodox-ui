58facf80f89820f14dda0cfd8693b5a26b7ec7be
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-28 08:18:42 -0400

    Added Letterbox toggle method

--- src/application/components/LetterBox/LetterBox.js
+++ src/application/components/LetterBox/LetterBox.js
@@ -1,4 +1,6 @@
 function LetterBox(opt) {
+  var self = this;
+
   this.isOpen = false;
 
   this.node = {
@@ -6,16 +8,32 @@ function LetterBox(opt) {
   };
 
   this.node.document.append(
+    this.node.trigger = el({ class : 'letterbox_trigger'},
+      el(Icon, 'menu')
+    ),
     this.node.container = el({ class : 'letterbox_container' })
   );
 
   this.target(this.node.container).append(
     this.node.search = el(Form.Search)
   );
+
+  this.node.trigger.on('click', function () {
+    self.toggle();
+  });
 }
 
 LetterBox.prototype.offset = function () {
   return this.node.document.offset();
+};
+
+LetterBox.prototype.toggle = function () {
+  if (this.isOpen) {
+    this.close();
+  } else {
+    this.open();
+  }
+  return this;
 };
 
 LetterBox.prototype.open = function () {
@@ -33,6 +51,24 @@ LetterBox.prototype.open = function () {
       self.node.search.focus();
     }
   });
+
+  this.trigger('open');
+};
+
+LetterBox.prototype.close = function () {
+  var offset = this.offset();
+  var self = this;
+
+  this.isOpen = false;
+
+  anime({
+    duration : 600,
+    targets : this.node.document.node,
+    right : [ -offset.width ],
+    easing : 'easeOutExpo'
+  });
+
+  this.trigger('close');
 };
 
 Component.extend(LetterBox);
