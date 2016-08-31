06399900fd06cbd11614a2aa71e4b2bf4b009410
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-31 08:25:02 -0400

    cleaning up setLongName

--- src/application/components/Key/key.prototype.setLongName.js
+++ src/application/components/Key/key.prototype.setLongName.js
@@ -1,41 +1,39 @@
 Key.prototype.setLongName = function () {
-  var keyCode = this.valueOf();
-
   // Long name
   if (this.parent) {
-    notLayer.keys[i].str_longName = 'Locked because it is dependent on a toggle.';
-  } else if (this.isHoldModifierTapKey) {
-    this.str_longName = KEYCODE.LONG_NAME[this.keyHold] + ' when held, ' + KEYCODE.LONG_NAME[this.keyTap] + ' when tapped.';
-  } else if (this.isHoldLayerTapKey) {
-    this.str_longName = 'switch to layer ' + this.keyHold + ' when held, ' + KEYCODE.PRIMARY[this.keyTap] + ' when tapped.';
-  } else if (this.isMomentLayer) {
-    this.str_longName = 'activates layer ' + this.keyHold + ' while held.';
-  } else if (this.isLayerToggle) {
-    this.str_longName = 'tap once to toggle "' + this.keyHold + '" layer tap again to toggle back.';
+    notLayer.keys[i].name = 'Locked because it is dependent on a toggle.';
+  } else if (isHoldModifierTapKey(this.keyCode)) {
+    this.name = KEYCODE[this.keyHold].name + ' when held, ' + KEYCODE[this.keyTap].name + ' when tapped.';
+  } else if (isHoldLayerTapKey(this.keyCode)) {
+    this.name = 'switch to layer ' + this.keyHold + ' when held, ' + KEYCODE[this.keyTap].primary + ' when tapped.';
+  } else if (isMomentLayer(this.keyCode)) {
+    this.name = 'activates layer ' + this.keyHold + ' while held.';
+  } else if (isLayerToggle(this.keyCode)) {
+    this.name = 'tap once to toggle "' + this.keyTap + '" layer, tap again to toggle back.';
   } else if (this.isModifiedKey) {
     if (
-      KEYCODE.isModifier(this.keyHold)
-      && KEYCODE.isModifier(this.keyTap)
+      isCommand(this.keyHold)
+      && isCommand(this.keyTap)
     ) {
-      this.str_longName = KEYCODE.PRIMARY['KC_' + this.keyHold] + ' + ' + KEYCODE.PRIMARY[this.keyTap] + ' when held.';
+      this.name = KEYCODE[this.keyHold].primary + ' + ' + KEYCODE[this.keyTap].primary + ' when held.';
     } else if (
-      KEYCODE.isModifier(this.keyHold)
-      && KEYCODE[this.keyHold]
-      && KEYCODE[this.keyHold][this.keyTap]
+      isCommand(this.keyHold)
+      && KEYCODE[this.keyTap]
+      && KEYCODE[this.keyTap].shift
     ) {
-      this.str_longName = KEYCODE[this.keyHold][this.keyTap] + ' when pressed.';
+      this.name = KEYCODE[this.keyTap].shift + ' when pressed.';
     } else {
-      this.str_longName = 'Unknown';
+      this.name = 'Unknown';
     }
   } else if (this.isFnKey) {
-    this.str_longName = 'Fn key \"' + keyCode.match(/KC_FN([0-9]+)/)[1] + '\" this function is defined in keymap.c';
-  } else if (this.isMacro) {
-    this.str_longName = 'macro \"' + parseInt(this.keyTap, 10) + '\"';
+    this.name = 'Function key \"' + this.args[1] + '\" this function is defined in keymap.c';
+  } else if (isMacro(this.keyCode)) {
+    this.name = 'macro \"' + this.args[1] + '\"';
   } else {
-    this.str_longName = KEYCODE.LONG_NAME[keyCode];
+    this.name = KEYCODE[this.keyCode].name;
   }
 
-  if (this.str_longName) {
-    this.str_longName = this.str_longName[0].toUpperCase() + this.str_longName.substr(1);
+  if (this.name) {
+    this.name = this.name[0].toUpperCase() + this.name.substr(1);
   }
 };
