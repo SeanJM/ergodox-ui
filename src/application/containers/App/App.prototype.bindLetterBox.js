a59e2c1ba351d5c414ff454b4b6522041ce171a9
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-09-06 10:14:51 -0400

    'bindLetterBox'

---
+++ src/application/containers/App/App.prototype.bindLetterBox.js
@@ -0,0 +1,32 @@
+App.prototype.bindLetterBox = function () {
+  var self = this;
+
+  this.node.layers.on('keyHover', function (e) {
+    self.node.status.value(e.target);
+  });
+
+  this.node.letterBox.on('open', function () {
+    var width = self.node.letterBox.offset().width;
+
+    anime({
+      duration : 600,
+      targets : self.node.workspace.node,
+      right : [ 0, width ],
+      easing : 'easeOutExpo'
+    });
+  });
+
+  this.node.letterBox.on('close', function () {
+    var width = self.node.letterBox.offset().width;
+
+    anime({
+      duration : 600,
+      targets : self.node.workspace.node,
+      right : [ 0 ],
+      easing : 'easeOutExpo'
+    });
+  });
+
+  this.node.layers.on('keyClick', function (e) {
+  });
+};
