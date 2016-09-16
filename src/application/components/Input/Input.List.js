555dbfdac8e077175740a0b3e82435137c9d813d
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-09-16 18:21:44 -0400

    Added 'tab index' so that the list could be 'focus'ed

--- src/application/components/Input/Input.List.js
+++ src/application/components/Input/Input.List.js
@@ -1,12 +1,14 @@
-Input.List = function () {
+Input.List = function (opt) {
   var self = this;
 
   this.node = {
     document : el(Input)
   };
 
+  this.list = [];
+
   this.node.document.append(
-    this.node.list = el({ class : 'input_list' })
+    this.node.list = el(_.assign({}, { class : 'input_list', tabIndex : 0 }, opt))
   );
 
   this.style = this.node.document.style;
@@ -14,15 +16,35 @@ Input.List = function () {
   this.on('mount', function () {
     self.node.document.trigger('mount');
   });
+
+};
+
+Input.List.prototype.select = function (n) {
+  this.selectedIndex = n;
+  this.list[n].select();
+  this.list
+    .filter((a, i) => i !== n)
+    .forEach(a => a.deselect());
 };
 
 Input.List.prototype.append = function () {
   var i = 0;
   var n = arguments.length;
   var target = this.target(this.node.list);
+  var self = this;
+
+  function select(e) {
+    self.select(self.list.indexOf(this));
+  }
+
   for (; i < n; i++) {
     target.append(arguments[i]);
+    if (arguments[i] instanceof Input.Item) {
+      this.list.push(arguments[i]);
+      arguments[i].on('select', select);
+    }
   }
+
 };
 
 Component.extend(Input.List);
