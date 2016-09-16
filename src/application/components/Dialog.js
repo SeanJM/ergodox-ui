c7e32ec1f87f955518760f2d66035dbffd5ffcee
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-09-16 18:09:22 -0400

    Dialog component will centre itself with window or x, y coordinates

--- src/application/components/Dialog.js
+++ src/application/components/Dialog.js
@@ -1,4 +1,6 @@
-function Dialog() {
+function Dialog(opt) {
+  var self = this;
+
   this.node = {
     document : el({ class : 'dialog' })
   };
@@ -8,24 +10,42 @@ function Dialog() {
     this.node.body = el({ class : 'dialog_body' }),
     this.node.feet = el({ class : 'dialog_feet' })
   );
-}
 
-Component.extend(Dialog);
+  this.on('open', function () {
+    var y = window.innerHeight / 2;
+    var x = window.innerWidth / 2;
+
+    this.node.document.appendTo(document.body);
+
+    if (opt && opt.x && opt.y) {
+      y = opt.y;
+      x = opt.x;
+    }
+
+    this.node.document.style.top = (
+      y - (this.node.document.offset().height / 2)
+    ) + 'px';
+
+    this.node.document.style.left = (
+      x - (this.node.document.offset().width / 2)
+    ) + 'px';
+  });
+
+  this.node.head.on('dragmove', function (e) {
+    self.node.document.style.transform = [
+      'translateX(' + e.detail.distanceX + 'px)',
+      'translateY(' + e.detail.distanceY + 'px)'
+    ].join(' ');
+  });
+}
 
 Dialog.prototype.append = function () {
   var i = 0;
   var n = arguments.length;
 
-  function onOpen(f) {
-    return function () {
-      f.focus();
-    };
-  }
-
   for (; i < n; i++) {
     if (arguments[i] instanceof Form) {
       this.node.body.append(arguments[i]);
-      this.on('open', onOpen(arguments[i]));
     } else if (arguments[i] instanceof Control) {
       this.node.feet.append(arguments[i]);
     } else if (arguments[i] instanceof Title) {
@@ -35,9 +55,7 @@ Dialog.prototype.append = function () {
 };
 
 Dialog.prototype.open = function () {
-  this.node.document.appendTo(document.body);
-  this.node.document.style.top = (
-    (window.innerHeight / 2) - (this.node.document.offset().height / 2)
-  ) + 'px';
   this.trigger('open');
 };
+
+Component.extend(Dialog);
