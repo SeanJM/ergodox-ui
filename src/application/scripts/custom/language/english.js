8d125e6c9c0e79693d42bb7460118d8699848ae2
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-09-16 18:22:02 -0400

    Language additions

--- src/application/scripts/custom/language/english.js
+++ src/application/scripts/custom/language/english.js
@@ -3,3 +3,8 @@ var lang = new Language('english');
 lang.set('duplicate layer name', 'Invalid layer name: **{{name}}**, this name is already taken. Please choose another name.');
 lang.set('confirm layer delete', 'Are you sure you want to delete layer **{{name}}**?');
 lang.set('error drop target key is locked', 'A layer toggle depends on this key. \nKeys are locked when their positions are referenced by layer toggles.');
+
+lang.set('select layer', 'Select Layer');
+
+lang.set('button ok', 'OK');
+lang.set('button cancel', 'Cancel');
