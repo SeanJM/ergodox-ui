dd7fb2841f067aac07981cb79b0d173b0f40b565
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-09-17 16:58:10 -0400

    Cleaned up 'Modal.ConfirmCancel' buttons

--- src/application/containers/Modal/Modal.ConfirmCancel.js
+++ src/application/containers/Modal/Modal.ConfirmCancel.js
@@ -1,34 +1,42 @@
 Modal.ConfirmCancel = function (opt) {
   var self = this;
 
-  this.node = el(Modal, {
-    text : opt.text,
-    icon : opt.icon
-  });
-
-  this.node.control(
-    el(Button.Confirm, {
-      onClick : function (e) {
-        if (typeof opt.onConfirm === 'function') {
-          // This is where form validation occurs
-          opt.onConfirm(e);
-        }
-        if (!e.defaultPrevented) {
-          self.node.close();
-        }
-      }
-    }),
-    el(Button.Cancel, {
-      onClick : function (e) {
-        if (typeof opt.onConfirm === 'function') {
-          self.node.close();
-          opt.onCancel(e);
-        }
-      }
-    })
+  var modal = el(
+    Modal, {
+      text : opt.text,
+      icon : opt.icon
+    }
   );
 
-  this.node.open();
+  this.node = {
+    document : modal
+  };
+
+  modal.node.control.append(
+    el(Button.Confirm),
+    el(Button.Cancel)
+  );
+
+  modal.node.control.on('confirm', function (e) {
+    if (typeof opt.onConfirm === 'function') {
+      // This is where form validation occurs
+      opt.onConfirm(e);
+    }
+
+    if (!e.defaultPrevented) {
+      modal.close();
+    }
+  });
+
+  modal.node.control.on('cancel', function (e) {
+    if (typeof opt.onCancel === 'function') {
+      opt.onCancel(e);
+    }
+
+    modal.close();
+  });
+
+  modal.open();
 };
 
 Component.extend(Modal.ConfirmCancel);
